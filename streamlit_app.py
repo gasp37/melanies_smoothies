d45a0509ce790ca1dc5d20229554919cfede061d
Pacheco <64242313+gasp37@users.noreply.github.com>
2026-09-15 16:10:00 -0300

    Fetch nutrition info for each selected fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,8 @@ if ingredients_list:
     # ingredients_string = ', '.join(ingredients_list) + '.'
     for fruit in ingredients_list:
         ingredients_string += fruit + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit + ' Nutrition Information')
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit}")
         sf_df =  st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
     st.write("You selected:")
