3c1880ec5c857bf2fa984c9764626a787fc9cde5
Pacheco <64242313+gasp37@users.noreply.github.com>
2026-09-15 16:04:24 -0300

    Display watermelon data in a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,4 @@ if ingredients_list:
         st.success(f'Your Smoothie is ordered, {customer_name}!', icon="✅")
       
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+sf_df =  st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
